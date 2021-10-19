87f877e6848949575b4246e4633cc0034e534670
Harsh-Modi278 <harshmodi278@gmail.com>
2021-10-19 23:51:14 +0530

    remove custome syntax highlighting

--- src/components/ReactMarkdownWrapper.js
+++ src/components/ReactMarkdownWrapper.js
@@ -11,28 +11,6 @@ const ReactMarkdownWrapper = ({ body }) => {
       children={body}
       linkTarget="_blank"
       remarkPlugins={[remarkGfm, remarkToc]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-          // className is the name of the language
-          // children contains the code
-          // props are the remainging props
-          return !inline && match ? (
-            <pre>
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, "")}
-                language={className}
-                style={docco}
-              />
-            </pre>
-          ) : (
-            //   If inline OR no match with language then just print the code
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
     />
   );
 };
