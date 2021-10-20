a93f74b9f2a5691ced0aee082f0ce42a351a7f8f
Harsh-Modi278 <harshmodi278@gmail.com>
2021-10-20 12:04:26 +0530

    fix: remove imports of syntax highlighters

--- src/components/ReactMarkdownWrapper.js
+++ src/components/ReactMarkdownWrapper.js
@@ -2,8 +2,6 @@ import { React } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkToc from "remark-toc";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const ReactMarkdownWrapper = ({ body }) => {
   return (
